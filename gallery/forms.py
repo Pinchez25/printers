af22461ca40c66a6f9568e38b2e46f987e5ac878
Pinchez25 <thuamwangi001@gmail.com>
2025-09-20 20:34:13 +0300

    add custom form for PortfolioItem with enhanced tag input widget

---
+++ gallery/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from taggit.forms import TagWidget
+from unfold.widgets import UnfoldAdminTextInputWidget
+
+from.models import PortfolioItem
+
+class UnfoldTagWidget(TagWidget):
+    """Custom tag widget that copies unfold's TextInput styling"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Create a temporary UnfoldAdminTextInputWidget to get its classes
+        temp_widget = UnfoldAdminTextInputWidget()
+        unfold_classes = temp_widget.attrs.get('class', '')
+
+        # Apply the same classes plus our placeholder
+        self.attrs.update({
+            'class': unfold_classes,
+            'placeholder': 'Add tags separated by commas',
+        })
+
+
+class PortfolioItemForm(forms.ModelForm):
+    class Meta:
+        model = PortfolioItem
+        fields = '__all__'
+        widgets = {
+            'tags': UnfoldTagWidget(),
+        }
