3b4302a608ed815a9ad4a42b80c09bac9392f108
Pinchez25 <thuamwangi001@gmail.com>
2025-09-26 16:03:15 +0300

    add context processor for global company configuration

---
+++ gallery/context_processors.py
@@ -0,0 +1,21 @@
+from django.utils.functional import SimpleLazyObject
+from django.core.exceptions import ImproperlyConfigured
+from django.db import OperationalError
+
+def _get_company_config():
+    """Helper function to get company config with error handling"""
+    try:
+        from gallery.models import CompanyConfig
+    except ImportError:
+        return None
+
+    try:
+        return CompanyConfig.get_instance()
+    except (CompanyConfig.DoesNotExist, OperationalError, ImproperlyConfigured):
+        return None
+
+def company_config(request):
+    """Context processor to make CompanyConfig available globally"""
+    return {
+        'config': SimpleLazyObject(_get_company_config)
+    }
