045545939574c791cfa77f35b98d6181d7f1ef41
Pinchez25 <thuamwangi001@gmail.com>
2025-09-26 15:59:57 +0300

    expand gallery and site URL routing with API endpoints and contact form integration

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from . import views
+
 app_name = 'gallery'
 
 urlpatterns = [
-    path("",TemplateView.as_view(template_name="index.html"),name="index"),
+    path("", views.index_view, name="index"),
+    path("gallery/", views.gallery_view, name="gallery"),
+    path("api/gallery/", views.gallery_api, name="gallery_api"),
+    path("api/gallery/tags/", views.gallery_tags_api, name="gallery_tags_api"),
+    path("contact/", views.contact_form_view, name="contact_form"),
 ]
