123e34d04c4469ee8a351552fc098878aca8b619
Pinchez25 <thuamwangi001@gmail.com>
2025-10-05 13:52:10 +0300

    update variables for dynamic values

--- gallery/context_processors.py
+++ gallery/context_processors.py
@@ -1,6 +1,7 @@
 from django.utils.functional import SimpleLazyObject
 from django.core.exceptions import ImproperlyConfigured
 from django.db import OperationalError
+from django.conf import settings
 
 def _get_company_config():
     """Helper function to get company config with error handling"""
@@ -15,7 +16,8 @@ def _get_company_config():
         return None
 
 def company_config(request):
-    """Context processor to make CompanyConfig available globally"""
+    """Context processor to make CompanyConfig and email settings available globally"""
     return {
-        'config': SimpleLazyObject(_get_company_config)
+        'config': SimpleLazyObject(_get_company_config),
+        'admin_email': settings.EMAIL_HOST_USER,
     }
