0bcf3dc17d49e4a4e4766a64f871623913e2e629
Pinchez25 <thuamwangi001@gmail.com>
2025-10-21 16:01:24 +0300

    refactor(gallery): change gallery view from single_page_view to index

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'gallery'
 
 urlpatterns = [
-    path("", views.single_page_view, name="index"),
+    path("", views.index, name="index"),
     path("api/gallery/", views.gallery_api, name="gallery_api"),
     path("api/gallery/tags/", views.gallery_tags_api, name="gallery_tags_api"),
     path("contact/", views.contact_form_view, name="contact_form"),
-    path('gallery/',views.single_page_view, name='gallery'),
+
 ]
